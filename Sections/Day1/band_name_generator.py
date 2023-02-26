94a672130a00c4c78ed8135b48b54c1d7484a625
Tsquared1 <109481517+Tsquared1@users.noreply.github.com>
2023-02-26 19:01:18 +0200

    Sections

--- Sections/Day1/band_name_generator.py
+++ Sections/Day1/band_name_generator.py
@@ -2,3 +2,4 @@ print("Welcome to the Band Name Generator!")
 city_name = input("What's the name of the city you grew up in?\n")
 pet_name = input("What's your pet's name?\n")
 print(f"Your band name could be {city_name} {pet_name}")
+# just a little change to update git
